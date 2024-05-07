834cfe7fd93ca771ad531ff635a45065fc00e245
ZiyanShabbir204 <ziyanwehvaria@gmail.com>
2024-05-08 02:20:47 +0500

    backend:  send message successfully

--- back-end/controllers/message.controller.mjs
+++ back-end/controllers/message.controller.mjs
@@ -1,4 +1,46 @@
-export const sendMessage = (req,res)=>{
-    console.log("message send",req.params.id)
+import Conversation from "../models/conversation.model.mjs"
+import Message from "../models/message.model.mjs"
+
+export const sendMessage = async (req,res)=>{
+    try {
+        const {message} = req.body
+        const {id:recieverId} = req.params
+        const senderId = req.user._id
+        // console.log("request user", req.user._id)
+
+        // console.log("sender id", senderId)
+        // console.log("reciverId ", recieverId)
+
+        let conversation = await Conversation.findOne({
+            participants : {$all : [recieverId,senderId]}
+        })
+        
+        if(!conversation){
+            conversation = await Conversation.create({
+                participants: [recieverId,senderId]
+            })
+        }
+
+        const newMessage = new Message({
+            recieverId,
+            senderId,
+            message
+        })
+
+        if(newMessage){
+            conversation.messages.push(newMessage._id)
+        }
+
+        await Promise.all([conversation.save(),newMessage.save()])
+
+        return res.status(201).json(newMessage)
+        
+
+        
+    } catch (error) {
+        console.log("error while creating message",error.message)
+        return res.status(500).json({error :"internal server error"})
+        
+    }
 
 }
