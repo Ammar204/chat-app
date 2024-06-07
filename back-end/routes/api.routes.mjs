9100dbe4fbaf5fdbf62d0854e4dfb7f31189d317
ZiyanShabbir204 <ziyanwehvaria@gmail.com>
2024-06-08 02:24:58 +0500

    Deployment

---
+++ back-end/routes/api.routes.mjs
@@ -0,0 +1,11 @@
+import express from "express"
+import authRoutes from "./auth.routes.mjs"
+import messageRoute from "./message.routes.mjs"
+import userRoute from "./user.routes.mjs"
+import { login,signup, logout } from "../controllers/auth.controller.mjs"
+const router = express.Router()
+
+router.use("/auth", authRoutes);
+router.use("/messages", messageRoute);
+router.use("/users", userRoute);
+export default router
--- back-end/server.mjs
+++ back-end/server.mjs
@@ -1,32 +1,36 @@
-import express from "express"
-import dotenv from "dotenv"
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.routes.mjs"
-import messageRoute from "./routes/message.routes.mjs"
-import userRoute from "./routes/user.routes.mjs"
-import cookieParser from "cookie-parser"
-import connectDb from "../database/connect-db.mjs"
-import {app,server,io} from "./sockets/socket.js"
-dotenv.config()
+import authRoutes from "./routes/auth.routes.mjs";
+import messageRoute from "./routes/message.routes.mjs";
+import userRoute from "./routes/user.routes.mjs";
+import cookieParser from "cookie-parser";
+import connectDb from "../database/connect-db.mjs";
+import { app, server, io } from "./sockets/socket.js";
+import apiRoutes from "./routes/api.routes.mjs"
+dotenv.config();
 
+app.use(express.json());
+app.use(cookieParser());
+const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 
-app.use(express.json())
-app.use(cookieParser())
-const PORT = process.env.PORT || 5000
+server.listen(PORT, () => {
+  {
+    connectDb();
 
-server.listen(PORT,()=>{{
+    console.log(`Your app is listening on ${PORT}`);
+  }
+});
 
-    connectDb()
+app.use("/api", apiRoutes);
+// app.use("/api/messages", messageRoute);
+// app.use("/api/users", userRoute);
 
-    console.log(`Your app is listening on ${PORT}`)
-}
-})
+app.use(express.static(path.join(__dirname, "front-end/dist")));
 
-app.get("/",(req,res)=>{
-    res.status(200).send("Hello World")
-})
-
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoute)
-app.use("/api/users",userRoute)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "front-end", "dist", "index.html"));
+});
