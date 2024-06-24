bafe7296f7d2af0c32c0fb49e8a062c5792b373c
Ammar Shabbir <ashabbir197@gmail.com>
2024-06-24 17:55:21 -0400

    removed frontend

--- back-end/routes/api.routes.mjs
+++ back-end/routes/api.routes.mjs
@@ -1,11 +1,14 @@
-import express from "express"
-import authRoutes from "./auth.routes.mjs"
-import messageRoute from "./message.routes.mjs"
-import userRoute from "./user.routes.mjs"
-import { login,signup, logout } from "../controllers/auth.controller.mjs"
-const router = express.Router()
+import express from "express";
+import authRoutes from "./auth.routes.mjs";
+import messageRoute from "./message.routes.mjs";
+import userRoute from "./user.routes.mjs";
+import { login, signup, logout } from "../controllers/auth.controller.mjs";
+const router = express.Router();
 
+router.get("/check-me", (req, res) => {
+  res.send("Hello world");
+});
 router.use("/auth", authRoutes);
 router.use("/messages", messageRoute);
 router.use("/users", userRoute);
-export default router
+export default router;
